1afc4380d2e563fe34b4a223086b7202f8e632cf
sithu <myintst@mytel.com.mm>
2026-06-15 14:53:43 +0630

    route list

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import AuthGate from './components/AuthGate'
@@ -12,9 +13,18 @@ if (!root) throw new Error('Root element not found')
 createRoot(root).render(
   <StrictMode>
     <Provider store={store}>
-      <AuthGate>
-        <App />
-      </AuthGate>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <AuthGate>
+                <App />
+              </AuthGate>
+            }
+          />
+        </Routes>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
